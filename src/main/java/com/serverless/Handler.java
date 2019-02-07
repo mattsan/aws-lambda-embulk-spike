96b11b2e5212918b42c5ae06d09fc93485815da2
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2019-02-07 20:28:18 +0900

    add plugins in codes

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -3,13 +3,17 @@ package com.serverless;
 import java.util.Collections;
 import java.util.Map;
 
-import org.apache.log4j.Logger;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.inject.Module;
+import com.google.inject.Binder;
+import org.apache.log4j.Logger;
 import org.embulk.EmbulkEmbed;
-import org.embulk.output.RedshiftOutputPlugin;
 import org.embulk.input.RedshiftInputPlugin;
+import org.embulk.output.RedshiftOutputPlugin;
+import org.embulk.plugin.InjectedPluginSource;
+import org.embulk.spi.InputPlugin;
+import org.embulk.spi.OutputPlugin;
 
 public class Handler implements RequestHandler<Map<String, Object>, Object> {
 
@@ -17,6 +21,27 @@ public class Handler implements RequestHandler<Map<String, Object>, Object> {
 
     @Override
     public Object handleRequest(Map<String, Object> input, Context context) {
-        return "OK";
+        EmbulkEmbed.Bootstrap bootstrap = new EmbulkEmbed.Bootstrap();
+        bootstrap.addModules(
+          new RedshiftInputModule(),
+          new RedshiftOutputModule()
+        );
+
+        EmbulkEmbed embulk = bootstrap.initializeCloseable();
+        return "Ya!";
+    }
+
+    static class RedshiftInputModule implements Module {
+        @Override
+        public void configure(Binder binder) {
+            InjectedPluginSource.registerPluginTo(binder, InputPlugin.class, "redfshiftInput", RedshiftInputPlugin.class);
+        }
+    }
+
+    static class RedshiftOutputModule implements Module {
+        @Override
+        public void configure(Binder binder) {
+            InjectedPluginSource.registerPluginTo(binder, OutputPlugin.class, "redfshiftOutput", RedshiftOutputPlugin.class);
+        }
     }
 }
