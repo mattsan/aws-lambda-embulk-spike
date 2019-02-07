e4b7b0b9ae59b23f508836011e5007e2c60c2a36
MATSUMOTO EIji <e-matsumoto@esm.co.jp>
2019-02-07 18:58:49 +0900

    import embulk plugins

--- src/main/java/com/serverless/Handler.java
+++ src/main/java/com/serverless/Handler.java
@@ -7,19 +7,16 @@ import org.apache.log4j.Logger;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.embulk.EmbulkEmbed;
+import org.embulk.output.RedshiftOutputPlugin;
+import org.embulk.input.RedshiftInputPlugin;
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+public class Handler implements RequestHandler<Map<String, Object>, Object> {
 
-	private static final Logger LOG = Logger.getLogger(Handler.class);
+    private static final Logger LOG = Logger.getLogger(Handler.class);
 
-	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
-		LOG.info("received: " + input);
-		Response responseBody = new Response("Go Serverless v1.x! Your function executed successfully!", input);
-		return ApiGatewayResponse.builder()
-				.setStatusCode(200)
-				.setObjectBody(responseBody)
-				.setHeaders(Collections.singletonMap("X-Powered-By", "AWS Lambda & serverless"))
-				.build();
-	}
+    @Override
+    public Object handleRequest(Map<String, Object> input, Context context) {
+        return "OK";
+    }
 }
--- src/main/java/com/serverless/Response.java
+++ src/main/java/com/serverless/Response.java
@@ -4,19 +4,19 @@ import java.util.Map;
 
 public class Response {
 
-	private final String message;
-	private final Map<String, Object> input;
+    private final String message;
+    private final Map<String, Object> input;
 
-	public Response(String message, Map<String, Object> input) {
-		this.message = message;
-		this.input = input;
-	}
+    public Response(String message, Map<String, Object> input) {
+        this.message = message;
+        this.input = input;
+    }
 
-	public String getMessage() {
-		return this.message;
-	}
+    public String getMessage() {
+        return this.message;
+    }
 
-	public Map<String, Object> getInput() {
-		return this.input;
-	}
+    public Map<String, Object> getInput() {
+        return this.input;
+    }
 }
